eec057b3d04b89d58ce93571f03fbd03d0e058cc
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-05-29 14:38:16 -0400

    added explanation field

--- public/index.js
+++ public/index.js
@@ -23,15 +23,18 @@ function addShopYearButtons(event) {
   const shops2018 = document.getElementById("shops-2018")
   shops2018.dataset.status = "inactive"
   shops2018.addEventListener('click', addShop2018Heatmap);
+  const explanation = document.getElementById("shops-expl")
 
-  if (event.target.innerText === "Pawn/Coffee Shops: OFF") {
-    shopsButton.innerText = "Pawn/Coffee Shops: ON"
+  if (event.target.innerText === "New Coffee Shops: OFF") {
+    shopsButton.innerText = "New Coffee Shops: ON"
     shops2010.style.display = "inline-block"
     shops2018.style.display = "inline-block"
-  } else if (event.target.innerText === "Pawn/Coffee Shops: ON") {
-    shopsButton.innerText = "Pawn/Coffee Shops: OFF"
+    explanation.style.display = "block"
+  } else if (event.target.innerText === "New Coffee Shops: ON") {
+    shopsButton.innerText = "New Coffee Shops: OFF"
     shops2010.style.display = "none"
     shops2018.style.display = "none"
+    explanation.style.display = "none"
   }
 }
 
@@ -47,7 +50,7 @@ function addShop2010Heatmap(event) {
       shop2010Heatmap = new google.maps.visualization.HeatmapLayer({
         data: locations,
         map: map,
-        maxIntensity: maxI,
+        maxIntensity: 4,
         radius: rad,
         opacity: opac
       })
@@ -71,7 +74,7 @@ function addShop2018Heatmap(event) {
       shop2018Heatmap = new google.maps.visualization.HeatmapLayer({
         data: locations,
         map: map,
-        maxIntensity: maxI,
+        maxIntensity: 4,
         radius: rad,
         opacity: opac
       })
@@ -94,22 +97,25 @@ function addNoiseYearButtons(event) {
   const noises2018 = document.getElementById("noises-2018")
   noises2018.dataset.status = "inactive"
   noises2018.addEventListener('click', addNoise2018Heatmap);
+  const explanation = document.getElementById("noise-expl")
 
   if (event.target.innerText === "Noise Complaints: OFF") {
     noisesButton.innerText = "Noise Complaints: ON"
     noises2010.style.display = "inline-block"
     noises2018.style.display = "inline-block"
+    explanation.style.display = "block"
   } else if (event.target.innerText === "Noise Complaints: ON") {
     noisesButton.innerText = "Noise Complaints: OFF"
     noises2010.style.display = "none"
     noises2018.style.display = "none"
+    explanation.style.display = "none"
   }
 }
 
 
 function addNoise2010Heatmap(event) {
   if (event.target.innerText === "2010" && event.target.parentNode.id === "noise-buttons" && event.target.dataset.status === "inactive") {
-    fetch('static/pawn_coffee2010.json')
+    fetch('static/noise2010.json')
     .then(res => res.json())
     .then(result => {
       let locations = result.map((val) => {
@@ -119,7 +125,7 @@ function addNoise2010Heatmap(event) {
         data: locations,
         map: map,
         maxIntensity: maxI,
-        radius: rad,
+        radius: 5,
         opacity: opac
       })
     })
@@ -132,7 +138,7 @@ function addNoise2010Heatmap(event) {
 
 function addNoise2018Heatmap(event) {
   if (event.target.innerText === "2018" && event.target.parentNode.id === "noise-buttons" && event.target.dataset.status === "inactive") {
-    fetch('static/pawn_coffee2018.json')
+    fetch('static/noise2018.json')
     .then(res => res.json())
     .then(result => {
       let locations = result.map((val) => {
@@ -142,7 +148,7 @@ function addNoise2018Heatmap(event) {
         data: locations,
         map: map,
         maxIntensity: maxI,
-        radius: rad,
+        radius: 5,
         opacity: opac
       })
     })
@@ -196,53 +202,53 @@ function addComment(event, id){
   .then(res => res.json())
   .then(comment => (slapItOnTheDiv(comment)))
 }
-
-/////////////////////////////FUNCTIONS//////////////////////////////////////////
-
-// Function to change the radius of data points on heatmap
-function changeRadius(bool) {
-  const step = 2, min = 0, max = 50;
-  let current = heatmap.get('radius');
-  let newValue = toggleUpDown(bool, current, step, min, max);
-
-  heatmap.set('radius', newValue);
-  document.getElementById("radiusNum").innerText = newValue;
-};
-
-// Function to change the opacity of the heatmap
-function changeOpacity(bool) {
-  const step = .2, min = 0, max = 1;
-  let current = heatmap.get('opacity');
-  let newValue = toggleUpDown(bool, current, step, min, max);
-  let rounded = round(newValue, 1);
-
-  heatmap.set('opacity', rounded);
-  document.getElementById("opacityNum").innerText = rounded;
-}
-
-// Function to change maxIntensity of the heatmap
-function changeIntensity(bool) {
-  const step = 10, min = 0, max = 1000;
-  let current = heatmap.get('maxIntensity');
-  let newValue = toggleUpDown(bool, current, step, min, max);
-
-  heatmap.set('maxIntensity', newValue);
-  document.getElementById("intensityNum").innerText = newValue;
-};
-
-// Changes our toggle values and keeps them within our min/max values
-function toggleUpDown(bool, current, step, min, max){
-  if (bool && current >= max) return current;
-  if (!bool && current <= min) return current;
-
-  if (bool) return current + step;
-  return current - step;
-}
-
-// Used to round the opacity toggle to one decimal place
-function round(value, precision) {
-  var multiplier = Math.pow(10, precision || 0);
-  return Math.round(value * multiplier) / multiplier;
-}
+//
+// /////////////////////////////FUNCTIONS//////////////////////////////////////////
+//
+// // Function to change the radius of data points on heatmap
+// function changeRadius(bool) {
+//   const step = 2, min = 0, max = 50;
+//   let current = heatmap.get('radius');
+//   let newValue = toggleUpDown(bool, current, step, min, max);
+//
+//   heatmap.set('radius', newValue);
+//   document.getElementById("radiusNum").innerText = newValue;
+// };
+//
+// // Function to change the opacity of the heatmap
+// function changeOpacity(bool) {
+//   const step = .2, min = 0, max = 1;
+//   let current = heatmap.get('opacity');
+//   let newValue = toggleUpDown(bool, current, step, min, max);
+//   let rounded = round(newValue, 1);
+//
+//   heatmap.set('opacity', rounded);
+//   document.getElementById("opacityNum").innerText = rounded;
+// }
+//
+// // Function to change maxIntensity of the heatmap
+// function changeIntensity(bool) {
+//   const step = 10, min = 0, max = 1000;
+//   let current = heatmap.get('maxIntensity');
+//   let newValue = toggleUpDown(bool, current, step, min, max);
+//
+//   heatmap.set('maxIntensity', newValue);
+//   document.getElementById("intensityNum").innerText = newValue;
+// };
+//
+// // Changes our toggle values and keeps them within our min/max values
+// function toggleUpDown(bool, current, step, min, max){
+//   if (bool && current >= max) return current;
+//   if (!bool && current <= min) return current;
+//
+//   if (bool) return current + step;
+//   return current - step;
+// }
+//
+// // Used to round the opacity toggle to one decimal place
+// function round(value, precision) {
+//   var multiplier = Math.pow(10, precision || 0);
+//   return Math.round(value * multiplier) / multiplier;
+// }
 
 fetchComments()
